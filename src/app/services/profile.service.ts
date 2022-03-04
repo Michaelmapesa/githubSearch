87f6e1dab6254dd713cd0b25271498c72cac3974
michael mapesa <michaelmapesa93@gmail.com>
2022-03-04 17:59:54 +0300

    creating profile services

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  private username = "string"; 
+  private clientid = '5ceeb84a143ff738faff';
+  private clientsecret = 'b542c7f42a6f0b81c984742b98aaf724c5a9d369';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    console.log("This service is now ready");
+    this.username = "Michaelmapesa"
+    
+
+   }
+   getProfileInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+     
+   }
 }
