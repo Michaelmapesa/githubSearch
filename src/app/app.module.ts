7416662dae16fa8869cd101868dbe1da0eac4769
michael mapesa <michaelmapesa93@gmail.com>
2022-03-12 12:06:28 +0300

    add repo-http service and service component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, SearchComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, SearchComponent, ServiceComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/repo-http/repos-request.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Repo } from '../repo-class/repo';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReposRequestService {
+  repo: Repo;
+
+  repos: [];
+
+  constructor(private http: HttpClient) {
+    this.repo = new Repo('', '', new Date()); 
+
+  }
+  async repoRequest(url: string, username: string) {
+    interface ApiResponse{
+      name: string;
+      repo_url: string;
+      created_at: Date;
+    }
+
+    let promise = new Promise((resolve,reject) => {
+      this.http
+      .get<ApiResponse>(
+        url + username + '/repos' + '?' + environment.access_token
+      )
+      .toPromise()
+      .then(
+        (response: any) => {
+          this.repos = response;
+
+          this.repos.forEach((element: Repo) => {
+            this.repo.name = element.name;
+            this.repo.html_url = element.html_url;
+            this.repo.created_at = new Date(element.created_at);
+          });
+
+          resolve('');
+        },
+        (err) => {
+          // this.repos = [];
+
+          reject(err);
+        }
+      ); 
+
+    });
+
+    return promise;
+  }
+}
