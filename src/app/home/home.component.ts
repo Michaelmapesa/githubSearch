b726f59d4d982bdc12d9db6df5e81224dbc482ee
michael mapesa <michaelmapesa93@gmail.com>
2022-03-12 11:09:24 +0300

    add home component file

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { User } from '../user-class/user';
+import { Repo } from '../repo-class/repo';
+
+import { UserRequestService } from '../user-http/user-request.service';
+import { ReposRequestService } from '../repos-http/repos-request.service';
+
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  users: User[];
+  user: User;
+
+  url: string = environment.userUrl;
+  username: string = '';
+  repos: Repo[];
+
+  constructor(
+    private userRequestService: UserRequestService,
+    private repoRequestService: ReposRequestService
+  ) {}
+
+  ngOnInit() {
+    this.searchUser();
+  }
+
+  name: string = '';
+  searchUser() {
+    this.userRequestService.userRequest(this.url, 'davidkiama');
+    this.user = this.userRequestService.user;
+
+    //the repos request took loonger to respond hence the timeout
+
+    this.repoRequestService.repoRequest(this.url, 'davidkiama');
+    setTimeout(() => {
+      this.repos = this.repoRequestService.repos;
+
+      console.log(this.repos);
+    }, 2000);
+  }
+}
